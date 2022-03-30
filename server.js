6977cd06cf3057205bc4a999d1c02b213d8e3cb1
jacksonnorris <jacksonnorr@gmail.com>
2022-03-29 22:32:00 -0400

    can now add roles with inquirer

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const { asyncScheduler } = require('rxjs');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -36,15 +37,12 @@ const addEmployee = () => {
                 rolesList.push(r[i].title);
             }
         });
-       
         db.query('SELECT * FROM employees', function (err, managers) {
             var m = managers;
             for (var j in m) {
                 managersList.push(m[j].first_name + " " + m[j].last_name);
             }
         });
-        
-
         const info = await inquirer.prompt([
             {
                 type: 'input',
@@ -72,9 +70,9 @@ const addEmployee = () => {
         let firstName = info.fName;
         let lastName = info.lName;
         let role = info.role;
-        var roleId = rolesList.indexOf(role);
+        var roleId = rolesList.indexOf(role) + 1;
         let manager = info.manager;
-        var managerId = managersList.indexOf(manager);
+        var managerId = managersList.indexOf(manager) + 1;
         db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, 
         [firstName, lastName, roleId, managerId],
         (err, result) => {
@@ -91,11 +89,59 @@ const showRoles = () => {
     initPrompt();
     });
 }
+const addRole = async () => {
+    var departmentList = [];
+        db.query('SELECT * FROM departments', function (err, departments) {
+            var d = departments;
+            for (var i in d) {
+                departmentList.push(d[i].name);
+            }
+        });
+    const rInfo = await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the first name of the role?',
+            name: 'rName'
+        },
+        {
+            type: 'input',
+            message: 'What is the salary of the role?',
+            name: 'salary'
+        },
+        {
+            type: 'list',
+            message: 'Which department does the role belong to?',
+            name: 'department',
+            choices: departmentList
+        }
+    ])
+    let roleName = rInfo.rName;
+    let salary = rInfo.salary;
+    let department = rInfo.department;
+    var departmentId = departmentList.indexOf(department) + 1;
+    db.query('INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)',
+    [roleName, salary, departmentId],
+    (err, result) => {
+        console.log(err);
+    });
+    initPrompt();
+}
+
 const showDepartments = () => { 
     db.query('SELECT * FROM departments', function (err, results) {
     console.table(results);
     initPrompt();
     });
+}
+
+const addDepartment = async () => {
+    const dInfo = await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'dName'
+        }
+    ])
 }
 
 app.use((req, res) => {
@@ -106,7 +152,7 @@ app.use((req, res) => {
     // console.log(`Server running on port ${PORT}`);
   });
 
-  const initQuestions = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add department', 'Quit'];
+  const initQuestions = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'];
 
   const initPrompt = async () => {
     const task = await inquirer.prompt([
@@ -128,6 +174,12 @@ app.use((req, res) => {
     }
     else if (task.task === 'Add Employee') {
         addEmployee();
+    }
+    else if (task.task === 'Add Role') {
+        addRole();
+    }
+    else if (task.task === 'Add Department') {
+        addDepartment();
     }
     
   }
